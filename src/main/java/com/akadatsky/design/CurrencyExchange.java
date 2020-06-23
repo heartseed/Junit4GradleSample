565e87e92d4148ac1f15b6f525e98901d555de5d
heartseed <heartseed.lqc@gmail.com>
2020-06-23 01:23:43 -0700

    add new tasks

---
+++ src/main/java/com/akadatsky/design/CurrencyExchange.java
@@ -0,0 +1,12 @@
+package com.akadatsky.design;
+
+public class CurrencyExchange {
+
+    /**
+     * Given a dict of { A->B: 100, A->C: 100, BTC->ETH: 5 }
+     *
+     * Get maxValue from source Coin --> target coin.
+     *
+     * Can use DFS / BFS / Dijstra / courseScehdule with cycle
+     * */
+}
---
+++ src/main/java/com/akadatsky/design/DesignSlack.java
@@ -0,0 +1,4 @@
+package com.akadatsky.design;
+
+public class DesignSlack {
+}
